aa9eee245dac0be73719209ca7645736e5542e3b
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-08 21:46:02 +0200

    add setup script

---
+++ data-raw/setup.R
@@ -0,0 +1,2 @@
+devtools::source_gist("654b9a85e2f005638d639f3e2f72bbb2")
+create_mc_package("rocketchatr", "Rocket.Chat R Client", "An R client to interact with a Rocket.Chat server.", "Mickaël Canouil")
