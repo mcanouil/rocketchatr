783ad71fe60226fd7a4fdcfce27da74af0e0a6b1
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-11 13:48:36 +0200

    add use_logo cmd

--- data-raw/setup.R
+++ data-raw/setup.R
@@ -1,2 +1,3 @@
 devtools::source_gist("654b9a85e2f005638d639f3e2f72bbb2")
 create_mc_package("rocketchatr", "Rocket.Chat R Client", "An R client to interact with a Rocket.Chat server.", "Mickaël Canouil")
+use_logo("https://raw.githubusercontent.com/mcanouil/hex-stickers/main/PNG/rocketchatr.png")
