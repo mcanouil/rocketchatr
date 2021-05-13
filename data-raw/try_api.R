912e783845a054f4cf204b6172c3b8f8d0c4d8a2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-05-13 14:53:24 +0200

    authenticate

--- data-raw/try_api.R
+++ data-raw/try_api.R
@@ -1,13 +1,7 @@
 # library(httr)
 
-rc_api <- function(
-  path,
-  url = "http://chat-new.egid.local:3000/",
-  user_agent = httr::user_agent("https://github.com/mcanouil/rocketchatr")
-) {
-  url <- httr::modify_url(url, path = path)
 
-  resp <- httr::GET(url, user_agent)
+rc <- function(resp, path) {
   if (httr::http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
@@ -35,6 +29,20 @@ rc_api <- function(
     class = "rocketchat_api"
   )
 }
+
+rc_api <- function(
+  path,
+  url = "http://chat-new.egid.local:3000/",
+  user_agent = httr::user_agent("https://github.com/mcanouil/rocketchatr")
+) {
+  url <- httr::modify_url(url, path = path)
+
+  resp <- httr::GET(url, user_agent)
+
+  rc(resp, path)
+}
+
+rc_api("api/info")
 
 
 authenticate <- function(
@@ -45,39 +53,16 @@ authenticate <- function(
   url <- httr::modify_url(url, path = path)
 
   resp <- httr::POST(
-    url = paste0(url, "?user=user&password=password"),
+    url = url,
     config = list(user_agent),
-    httr::authenticate(
+    body = jsonlite::toJSON(list(
       user = rstudioapi::askForPassword("User"),
       password = rstudioapi::askForPassword("password")
-    )
-    # list(user = user, password = password)
+    ), auto_unbox = TRUE),
+    encode = "raw"
   )
 
-  if (httr::http_type(resp) != "application/json") {
-    stop("API did not return json", call. = FALSE)
-  }
-
-  parsed <- jsonlite::fromJSON(httr::content(resp, "text"), simplifyVector = FALSE)
-
-  if (httr::status_code(resp) != 200) {
-    stop(
-      sprintf(
-        "REST API request failed [%s]\n%s\n<%s>",
-        httr::status_code(resp),
-        parsed$message,
-        parsed$documentation_url
-      ),
-      call. = FALSE
-    )
-  }
-
-  structure(
-    list(
-      content = parsed,
-      path = path,
-      response = resp
-    ),
-    class = "rocketchat_api"
-  )
+  rc(resp, path)
 }
+
+authenticate()
